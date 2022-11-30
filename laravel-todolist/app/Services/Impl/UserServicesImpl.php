d2f6188504a63bde96e6c3ed7694536683ed0f72
wahyupambudi <wahyupambudi823@gmail.com>
2022-11-30 17:28:57 +0700

    service implement function login

--- laravel-todolist/app/Services/Impl/UserServicesImpl.php
+++ laravel-todolist/app/Services/Impl/UserServicesImpl.php
@@ -5,6 +5,22 @@ use App\Services\UserServices;
 
 class UserServicesImpl implements UserServices {
 
+    private array $users = [
+        "wahyu" => "rahasia"
+    ];
+    
+    function login(string $user, string $password): bool {
+        if(!isset($this->users[$user])) {
+            return false;
+        }
 
+        $correctPassword = $this->users[$user];
+        return $password == $correctPassword;
+
+        // if($password == $correctPassword) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+    }
 }
-?>
