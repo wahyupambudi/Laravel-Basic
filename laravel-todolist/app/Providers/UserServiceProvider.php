fdbcfd0ee367e8515e5751eb2129a8805d61f534
wahyupambudi <wahyupambudi823@gmail.com>
2022-11-30 17:27:54 +0700

    public array singletons n provides

--- laravel-todolist/app/Providers/UserServiceProvider.php
+++ laravel-todolist/app/Providers/UserServiceProvider.php
@@ -2,10 +2,25 @@
 
 namespace App\Providers;
 
+use App\Services\Impl\UserServicesImpl;
+use App\Services\UserServices;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
 class UserServiceProvider extends ServiceProvider implements DeferrableProvider
 {
+
+    // membuat public array singletons
+    public array $singletons = [
+        UserServices::class => UserServicesImpl::class
+    ];
+
+    // membuat function provides
+    public function provides(): array
+    {
+        return [UserServices::class];
+    }
+
     /**
      * Register services.
      *
